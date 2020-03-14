b4b40384de356344f354effd8bbedb5ffe77e729
surendra93960 <surendrareddyssud93960@gmail.com>
2020-03-14 16:41:07 +0530

    just started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { } from 
+import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -9,8 +9,15 @@ import { SigninScreenComponent } from './signin-screen/signin-screen.component';
 import { SignupScreenComponent } from './signup-screen/signup-screen.component';
 
 
+const appRoutes : Routes = [
+  { path : '' , component : HomeComponent},
+  { path : 'home' , component : HomeComponent},
+  { path : 'signin' , component : SigninScreenComponent},
+  { path : 'signup' , component : SignupScreenComponent},
+] 
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule ,RouterModule.forRoot(appRoutes) ],
   declarations: [ AppComponent, NavbarComponent, HomeComponent, SigninScreenComponent, SignupScreenComponent,  ],
   bootstrap:    [ AppComponent ]
 })
